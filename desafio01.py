23e6925aff8e2ed645c973ef3c0a55ffa648761a
CarowlCarvalho <62719696+CarowlCarvalho@users.noreply.github.com>
2020-04-28 10:37:03 -0300

    Add files via upload
    Aulas práticas

---
+++ desafio01.py
@@ -0,0 +1,19 @@
+
+
+nome = input ('Qual é o seu nome? ')
+print('Olá', nome , '! Prazer em te conhecer!')
+
+
+
+
+dia = input ('Qual é o dia do seu aniversário? ')
+mês = input ('Qual é o mês do seu aniversário? ')
+ano = input ('Em qual ano você nasceu? ')
+print ('Você nasceu no dia', dia ,' de' ,mês, 'de' ,ano, '.Correto?')
+
+N1 = input ('Digite um número: ')
+n2 = input ('Digite outro número: ')
+soma = N1 + n2
+print (' A Soma de' , N1 + n2 , 'é' ,soma,'.') 
+
+

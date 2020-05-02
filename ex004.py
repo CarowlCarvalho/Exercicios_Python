3c85aa685c82e38713f26aba90f30a2aa8d160d5
Carowl Carvalho <62719696+CarowlCarvalho@users.noreply.github.com>
2020-05-02 13:54:18 -0300

    Add files via upload
    Dissecando variáveis, essa é uma versão do desafio 04

---
+++ ex004.py
@@ -0,0 +1,8 @@
+a = input('Digite algo: ')
+print('O tipo primitivo desse valor é', type(a))
+print('Só tem espaços: ', a.isspace())
+print('É um número?', a.isalnum())
+print('É alfabético? ', a.isalpha())
+print('É alfanumérico? ', a.isalnum())
+print('Ele esta em maiúscula? ', a.isupper())
+print('Ele esta em minúsculas? ', a.islower())
